35d04130073fddb58751745d2a52316b647fef81
Sumaiya Rimu <sumaiyarimu22@gmail.com>
2023-02-24 15:29:50 +0600

    validator added

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt"); //normal password encrypt for use hasing and use laibary(bcrypt)
+const validator = require("validator"); //check email,password ok or not
 
 const Schema = mongoose.Schema;
 
@@ -16,6 +17,22 @@ const userSchema = new Schema({
 });
 
 userSchema.statics.signup = async function (email, password) {
+  //validation
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  if (!validator.isEmail(email)) {
+    throw Error("Invalid email");
+  }
+
+  //Lowercase, uppercase, number, 8+ chars
+  if (!validator.isStrongPassword(password)) {
+    throw Error(
+      "Password is not strong try to combined Lowercase, uppercase, number, 8+ chars"
+    );
+  }
+
   const exist = await this.findOne({ email });
 
   if (exist) {
